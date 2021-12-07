c6cd052831ee29a1657ba02e5f8e7230275ef204
Colin Ramsay <ctramsay@uw.edu>
2021-12-07 04:18:50 -0800

    completed detail view and assignment 8

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blogging.views import PostListView, stub_view, list_view, detail_view
+from blogging.views import PostDetailView, PostListView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
-    path("posts/<int:post_id>/", detail_view, name="blog_detail"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -10,34 +10,17 @@ class PostListView(ListView):
     queryset = published.order_by("-published_date")
     template_name = "blogging/list.html"
 
-class BloggingDetailView(DetailView):
-    published = Post.objects.exclude(published_date__exact=None)
-    queryset = published.order_by("-published_date")
+class PostDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+    def get(self, request, *args, **kwargs):
+        try:
+            post = self.get_object()
+        except Post.DoesNotExist:
+            raise Http404
+        context = {"object": post}
+        return render(request, "blogging/detail.html", context)
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by("-published_date")
-    context = {"posts": posts}
-    return render(request, "blogging/list.html", context)
 
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {"post": post}
-    return render(request, "blogging/detail.html", context)
